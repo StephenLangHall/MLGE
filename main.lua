78884db61fd93830bd884a978f225c0cff406f8a
step <stephenlh2007@gmail.com>
2024-01-11 14:32:42 -0500

    Adding seperate main file for Game

---
+++ main.lua
@@ -0,0 +1,27 @@
+return {
+  ["Main"]=function()
+    Clear("black")
+	  local Mex=10
+	  local Mey=10
+	  for n,v in pairs(Things) do
+  		v.draw()
+	  end
+  end,
+  ["Things"]={
+	  ["Me"]={
+		  ["x"]=10,
+		  ["y"]=10,
+		  ["draw"]=(function()
+		  	local Mex=Things.Me.x
+			  local Mey=Things.Me.y
+			  Rectangle(Mex, Mey, 2, 6, "white", 1)
+			  Pixel(Mex-1, Mey+2, "white")
+			  Pixel(Mex+3, Mey+2, "white")
+			  Pixel(Mex,   Mey+1, "green")
+			  Pixel(Mex+2, Mey+1, "green")
+			  Pixel(Mex,   Mey+7, "white")
+			  Pixel(Mex+2, Mey+7, "white")
+		  end),
+	  },
+  },
+}
